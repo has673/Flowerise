8db0e906c06cc2b6edb700dc5c02add024011c20
has673 <hassansajjas>
2024-07-29 11:46:24 +0500

    d

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="display-flex">
+        <ul>
+        
+          <li>About</li>
+          <li>Features</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
