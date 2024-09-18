7f4f41733b0796ad07b8be827a48c89b9c58ad56
has673 <hassansajjas>
2024-09-18 15:13:27 +0500

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPWA from "next-pwa";
 
-export default nextConfig;
+const nextConfig = {
+  // Add your Next.js configuration here
+};
+
+const withPWA = nextPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+export default {
+  ...nextConfig, // Merge with your existing Next.js config
+  ...withPWA, // Add PWA configuration
+};
